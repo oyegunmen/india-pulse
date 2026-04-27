24ad74c89861ce14d4115503515f669d531dbce0
cos-droid <dikshitdesign@gmail.com>
2026-04-27 22:01:51 +0530

    Python Fix

--- aggregator.py
+++ aggregator.py
@@ -2,7 +2,7 @@ import feedparser
 import json
 from datetime import datetime, timedelta, timezone
 
-HEADERS = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
 
 ist_tz = timezone(timedelta(hours=5, minutes=30))
 
